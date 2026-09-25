ebfac74659d221c7e2d8b119fd7a5677785cf3a9
FomaAkvinsky <avavto@gmail.com>
2026-09-26 01:43:45 +0600

    chore: bump frontend asset version

--- public/includes/head.php
+++ public/includes/head.php
@@ -21,8 +21,8 @@ $pageCanonical = $pageCanonical ?? 'https://school.askoza.ru/';
   <meta property="og:description" content="<?= htmlspecialchars($pageDescription, ENT_QUOTES, 'UTF-8') ?>">
   <meta property="og:url" content="<?= htmlspecialchars($pageCanonical, ENT_QUOTES, 'UTF-8') ?>">
 
-  <link rel="stylesheet" href="/assets/css/style.css?v=1">
-  <script defer src="/assets/js/main.js?v=1"></script>
+  <link rel="stylesheet" href="/assets/css/style.css?v=2">
+  <script defer src="/assets/js/main.js?v=2"></script>
 
   <script type="application/ld+json">
   {
